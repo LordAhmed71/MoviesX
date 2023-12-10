3350a21827ac9eb1972484fad4a871ede74ac655
AhmedSaid <ahmed55mohamed88@gmail.com>
2023-12-10 21:47:35 +0200

    React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { fetch } from "./Utils/api";
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfiguration } from "./Store/homeSlice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
@@ -22,7 +23,17 @@ const App = () => {
     fetch("/movie/popular").then((res) => dispatch(getApiConfiguration(res)));
   };
 
-  return <div>{url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
