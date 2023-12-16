b5504fc5db0ebec3a9200e4c79787462f3e1e2ef
AhmedSaid <ahmed55mohamed88@gmail.com>
2023-12-16 11:50:37 +0200

    add slide function

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -21,6 +21,16 @@ const Carousel = ({ data, loading }) => {
 
   const navigation = (dir) => {
     const container = carouselContainer.current;
+
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
   };
 
   const skItem = () => {
@@ -46,13 +56,17 @@ const Carousel = ({ data, loading }) => {
           onClick={() => navigation("right")}
         />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((movie) => {
               const posterUrl = movie?.poster_path
                 ? url.poster + movie?.poster_path
                 : PosterFallback;
               return (
-                <div className="carouselItem" key={movie.id}>
+                <div
+                  className="carouselItem"
+                  key={movie.id}
+                  onClick={() => navigate(`/${movie.media_type}/${movie.id}`)}
+                >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={movie.vote_average.toFixed(1)} />
