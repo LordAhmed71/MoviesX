9ddbcb502a5275f17f08bfa59fb99705bee08486
AhmedSaid <ahmed55mohamed88@gmail.com>
2023-12-17 14:18:37 +0200

    search function

---
+++ src/Components/MovieCard/MovieCard.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import Img from "../lazyLoadImage/Img";
+import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
+import PosterFallback from "../../assets/no-poster.png";
+import { formatDate } from "../../Utils/helpers";
+
+import "./MovieCard.scss";
+const MovieCard = ({ data, fromSearch, mediaType }) => {
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
+  const posterUrl = data.poster_path
+    ? url.poster + data.poster_path
+    : PosterFallback;
+  return (
+    <div
+      className="movieCard"
+      onClick={() => navigate(`/${data.media_type || mediaType}/${data.id}`)}
+    >
+      <div className="posterBlock">
+        <Img className="posterImg" src={posterUrl} />
+        {!fromSearch && (
+          <React.Fragment>
+            <CircleRating rating={data.vote_average.toFixed(1)} />
+            <Genres data={data.genre_ids.slice(0, 2)} />
+          </React.Fragment>
+        )}
+      </div>
+      <div className="textBlock">
+        <span className="title">{data.title || data.name}</span>
+        <span className="date">{formatDate(data.release_date)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/Pages/SearchResult/SearchResult.jsx
+++ src/Pages/SearchResult/SearchResult.jsx
@@ -1,7 +1,81 @@
-import "./SearchResult.scss";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchDataFromApi } from "./../../Utils/api";
+import ContentWrapper from "./../../Components/ContentWrapper/ContentWrapper";
+import noResult from "../../assets/no-results.png";
 
+import "./SearchResult.scss";
+import Spinner from "../../Components/Spinner/Spinner";
+import MovieCard from "../../Components/MovieCard/MovieCard";
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+
+  const fetchInitialData = () => {
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((pre) => pre + 1);
+        setLoading(false);
+      }
+    );
+  };
+  const fetchNextPageData = () => {
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        if (data?.results) {
+          setData({
+            ...data?.results,
+            results: [...data?.results, ...res?.results],
+          });
+        } else {
+          setData(res);
+        }
+        setPageNum((pre) => pre + 1);
+      }
+    );
+  };
+  useEffect(() => {
+    setPageNum(1);
+    fetchInitialData();
+  }, [query]);
+  return (
+    <div className="searchResultsPage">
+      {loading && <Spinner />}
+
+      {!loading && (
+        <ContentWrapper>
+          {data?.results?.length > 0 ? (
+            <>
+              <div className="pageTitle">
+                {`Search ${
+                  data?.total_results > 1 ? "results" : "result"
+                } of '${query}'`}
+              </div>
+              <InfiniteScroll
+                className="content"
+                dataLength={data?.results?.length || []}
+                next={fetchNextPageData}
+                hasMore={pageNum <= data?.total_pages}
+                loader={<Spinner />}
+              >
+                {data.results.map((item, i) => {
+                  if (item.media_type === "person") return;
+                  return <MovieCard key={i} data={item} fromSearch={true} />;
+                })}
+              </InfiniteScroll>
+            </>
+          ) : (
+            <span className="resultNotFound">Sorry, result not found!</span>
+          )}
+        </ContentWrapper>
+      )}
+    </div>
+  );
 };
 
 export default SearchResult;
